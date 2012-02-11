ef28d4dfabbe0ef2cc07a6b185f566dfcaeaad34
Jayan <jcabatuan@gmail.com>
2012-02-11 21:29:35 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BloopPh::Application.config.secret_token = '96330b711df90087161481316e9825935da1494e31a96e2342ba33f1cbcb2f7010c5ee00c246c7397621e2c62cec78643874212f6fbb72b5ee8b63034bf3efc0'
